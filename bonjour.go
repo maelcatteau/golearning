006fc9d1c09411294ce56b30df7d000e95224453
Maël CATTEAU <mael.catteau@gmail.com>
2025-08-24 15:28:31 +0200

    Ajout des langues

--- bonjour.go
+++ bonjour.go
@@ -2,10 +2,33 @@ package main
 
 import "fmt"
 
-func Bonjour(nom string) string {
-	return "Bonjour, " + nom
+const anglais = "Anglais"
+const francais = "Français"
+const espagnol = "Espagnol"
+
+const prefixeSalutAnglais = "Hello, "
+const prefixeSalutFrancais = "Bonjour, "
+const prefixeSalutEspagnol = "Hola, "
+
+func Bonjour(nom string, langue string) string {
+	if nom == "" {
+		nom = "Monde"
+	}
+	return prefixeSalut(langue) + nom
+}
+
+func prefixeSalut(langue string) (prefixe string) {
+	switch langue {
+	case anglais:
+		prefixe = prefixeSalutAnglais
+	case espagnol:
+		prefixe = prefixeSalutEspagnol
+	default:
+		prefixe = prefixeSalutFrancais
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Bonjour("Chris"))
+	fmt.Println(Bonjour("Chris", ""))
 }
--- bonjour_test.go
+++ bonjour_test.go
@@ -3,10 +3,33 @@ package main
 import "testing"
 
 func TestBonjour(t *testing.T) {
-	resultat := Bonjour("Chris")
-	attendu := "Bonjour, Chris"
 
-	if resultat != attendu {
-		t.Errorf("reçu %q attendu %q", resultat, attendu)
+	verifierMessageCorrect := func(t testing.TB, resultat, attendu string) {
+		t.Helper()
+		if resultat != attendu {
+			t.Errorf("reçu %q attendu %q", resultat, attendu)
+		}
 	}
+	t.Run("dire bonjour aux personnes", func(t *testing.T) {
+		resultat := Bonjour("Chris", "Francais")
+		attendu := "Bonjour, Chris"
+		verifierMessageCorrect(t, resultat, attendu)
+	})
+	t.Run("dire bonjour Monde quand une chaine vide est fournie, verifie egalement la langue par défaut", func(t *testing.T) {
+		resultat := Bonjour("", "")
+		attendu := "Bonjour, Monde"
+		verifierMessageCorrect(t, resultat, attendu)
+	})
+
+	t.Run("en espagnol", func(t *testing.T) {
+		resultat := Bonjour("Elodie", "Espagnol")
+		attendu := "Hola, Elodie"
+		verifierMessageCorrect(t, resultat, attendu)
+	})
+	t.Run("en anglais", func(t *testing.T) {
+		resultat := Bonjour("Elodie", "Anglais")
+		attendu := "Hello, Elodie"
+		verifierMessageCorrect(t, resultat, attendu)
+	})
+
 }
